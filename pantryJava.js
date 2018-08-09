4237839d2b59acdbc03a41f2663ddb2a7f9df65e
Jason <lovrienj@oregonstate.edu>
2018-08-08 20:58:20 -0700

    Stuff was done javascript isn't working because reasons

---
+++ pantryJava.js
@@ -0,0 +1,11 @@
+
+window.alert("HEY YOUR JAVASCRIPT IS HERE");
+
+var addButton = document.getElementById("addButton");
+
+addButton.addEventListener("click", function(event){
+  var popUp = document.getElementById("addItemPopup");
+   popUp.style.visibility = "hidden";
+    
+};
+);
