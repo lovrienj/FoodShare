6b15cb6c07996269deeda6e490e1dd5483194ff4
patrickgsmith <patrickgromersmith@gmail.com>
2018-08-10 15:48:23 -0700

    Changed file structure, added Dexie embed

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var bs = require('browser-sync').create();
+
+gulp.task('bs', function(){
+  bs.init({
+    server: ["public", "views"]
+  });
+});
+
+gulp.task('watch', ['bs'], function(){
+  gulp.watch("views/*.html").on("change", bs.reload);
+  gulp.watch("public/js/*.js").on("change", bs.reload); 
+  gulp.watch("public/css/*.css").on("change", bs.reload);
+});
