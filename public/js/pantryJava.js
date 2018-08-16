3a98c0d87e2051b1ff29a174556da3735f645326
patrickgsmith <patrickgromersmith@gmail.com>
2018-08-16 14:25:18 -0700

    a little refactoring

--- public/js/pantryJava.js
+++ public/js/pantryJava.js
@@ -1,4 +1,3 @@
-// Animation functions
 function slideOut(elem, duration){
     elem.style.transition = "all " + duration + "ms";
     elem.style.opacity = 0;
@@ -9,47 +8,6 @@ function slideOut(elem, duration){
     }, duration);
 }
 
-
-// DOM Selections
-var addButtonItem = document.getElementById("addButton");
-var popUp = document.getElementById("addItemPopup");
-var submitButton = document.getElementById("submitButton");
-const pantryItemDisplay = document.getElementById("pantryItemDisplay");
-
-addButtonItem.addEventListener("click", function(event){
-   popUp.style.visibility = "visible";
-});
-
-submitButton.addEventListener("click", function(event){
-    var foodInput = document.getElementById("foodName"),
-        expoDate = document.getElementById("expoDate");
-    
-    if(foodInput.value == "") return; //not allowing blank items
-    popUp.style.visibility = "hidden";
-    addFoodItem(foodInput.value, expoDate.value);
-});
-
-
-// This event listener is on the whole pantry div
-// When anywhere on the pantry is clicked, this gets triggered. 
-// It then checks the id of what actually fired, and 
-// if it was a delete button, then it will delete that item from the DB
-pantryItemDisplay.addEventListener('click', function(event){
-    if(event.target.classList.contains("delete"))
-    {
-        removePantryItem(event.target);
-        // var itemToDeleteStr = event.target.getAttribute("buttonItem");
-        // var itemToDelete = Number(itemToDeleteStr);
-
-        // console.log("Trying to delete item w/ id ", itemToDelete);
-        // //help with delete from: https://stackoverflow.com/questions/46497137/dexie-js-table-deleteid-not-working-for-per-row-deletion
-        // db.foodItems.where(":id").equals(itemToDelete).delete()
-        //     .then(()=>{
-        //         slideOut(event.target.parentNode, 200);
-        //     });
-    }
-});
-
 function removePantryItem(elem){
     var itemToDelete = Number(elem.getAttribute("buttonItem"));
 
@@ -57,13 +15,30 @@ function removePantryItem(elem){
     //help with delete from: https://stackoverflow.com/questions/46497137/dexie-js-table-deleteid-not-working-for-per-row-deletion
     db.foodItems.where(":id").equals(itemToDelete).delete()
         .then(()=>{
-            slideOut(event.target.parentNode, 200);
+            slideOut(elem.parentNode, 200);
         });    
 }
 
 function formatDate(unformattedDate){
     let splitDate = unformattedDate.split("-");
-    return `${splitDate[1].replace(/0(?=[1-9])/, "")}-${splitDate[2].replace(/0(?=[1-9])/, "")}-${splitDate[0].slice(2)}`;
+    return `${splitDate[1].replace(/^0/, "")}-${splitDate[2].replace(/^0/, "")}-${splitDate[0].slice(2)}`;
+}
+
+function expirationCheck(item){
+    var today = new Date();
+    var expirationtime= new Date(item.expoDate);
+    let daystoExpiration = Math.abs(today.gettime()-expirationtime.gettime()); 
+    var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
+    alert(diffDays);
+    //should then write a loop to go through all the foods in pantry and print it 
+}
+
+
+function putWrapper(param){
+    console.log("put called with param:");
+    console.dir(param);
+
+    return  db.foodItems.put(param);
 }
 
 
@@ -74,7 +49,15 @@ function formatDate(unformattedDate){
 */
 function addFoodItem(name, expoDate){
     
-    db.foodItems.put({name: name, expoDate: expoDate}).then(function(id){
+    // db.foodItems.put({name: name, expoDate: expoDate}).then(function(id){
+    //     return db.foodItems.get(id);
+    // }).then(function (foodItem){
+    //     //after adding item, adding to display
+    //     createPantryItem(foodItem);
+    // }).catch(function(error) {
+    //     alert ("Ooops: " + error);
+    // });  
+    putWrapper({name: name, expoDate: expoDate}).then(function(id){
         return db.foodItems.get(id);
     }).then(function (foodItem){
         //after adding item, adding to display
@@ -83,8 +66,6 @@ function addFoodItem(name, expoDate){
         alert ("Ooops: " + error);
     });  
 }
-
-
 
 function createPantryItem(item){
     console.dir(item);
@@ -112,7 +93,7 @@ function createPantryItem(item){
 }
 
 function displayPantry(){
-    /*r
+    /*
     For each item in DB
     make flex box
     and put in flex container
@@ -122,11 +103,44 @@ function displayPantry(){
 
     let newElement;
     db.foodItems.each(item => {
+        console.log("in display each loop");
         createPantryItem(item);
     })  
 }
 
-/* Example from Dexie Docs */
+// DOM Selections
+var addButtonItem = document.getElementById("addButton");
+var popUp = document.getElementById("addItemPopup");
+var submitButton = document.getElementById("submitButton");
+const pantryItemDisplay = document.getElementById("pantryItemDisplay");
+
+
+// Event Listeners
+addButtonItem.addEventListener("click", function(event){
+   popUp.style.visibility = "visible";
+});
+
+submitButton.addEventListener("click", function(event){
+    console.log("clickin'");
+    
+    var foodInput = document.getElementById("foodName"),
+        expoDate = document.getElementById("expoDate");
+    
+    if(foodInput.value == "") return; //not allowing blank items
+    popUp.style.visibility = "hidden";
+    addFoodItem(foodInput.value, expoDate.value);
+});
+
+// This event listener is on the whole pantry div
+// When anywhere on the pantry is clicked, this gets triggered. 
+// It then checks the id of what actually fired, and 
+// if it was a delete button, then it will delete that item from the DB
+pantryItemDisplay.addEventListener('click', function(event){
+    if(event.target.classList.contains("delete"))
+    {
+        removePantryItem(event.target);
+    }
+});
 
 // Define the database
 const db = new Dexie("foodshare_database");
@@ -134,24 +148,5 @@ db.version(1).stores({
     foodItems: 'id++,name,expoDate'
 });
 
-// displayPantry();
-
-// console.log(db);
-
-// // Put some data into it
-// db.foodItems.put({name: "Banana"}).then(function(id){
-//     return db.foodItems.get(id);
-// }).then(function (foodItem){
-//     console.log(foodItem);
-//     alert ("Banana expires on" + foodItem.name);
-// }).catch(function(error) {
-//     alert ("Ooops: " + error);
-// });
-function expirationCheck(item){
-    var today = new Date();
-    var expirationtime= new Date(item.expoDate);
-    let daystoExpiration = Math.abs(today.gettime()-expirationtime.gettime()); 
-    var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
-    alert(diffDays);
-    //should then write a loop to go through all the foods in pantry and print it 
-}
+// Display the current pantry
+displayPantry();
